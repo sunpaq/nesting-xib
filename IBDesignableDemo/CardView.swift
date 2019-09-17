6296d72e5f66c8a553c8855784eb14f8e7bcab25
sunyuli <sunpaq@gmail.com>
2019-09-17 23:04:13 +0900

    update

---
+++ IBDesignableDemo/CardView.swift
@@ -0,0 +1,13 @@
+//
+//  CardView.swift
+//  IBDesignableDemo
+//
+//  Created by 孙御礼 on R 1/09/17.
+//  Copyright © Reiwa 1 oreisoft. All rights reserved.
+//
+
+import UIKit
+
+class CardView: NibView {
+
+}
--- IBDesignableDemo/NibView.swift
+++ IBDesignableDemo/NibView.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
+@IBDesignable
 class NibView: UIView {
 
-    @IBOutlet weak var label: UILabel!
-    
-    @IBOutlet weak var button: UIButton!
-    
-    override class func prepareForInterfaceBuilder() {
+    //MARK: - Interface Builder Debug
+    @IBInspectable var doesDrawPreview: Bool = false
 
+    @IBInspectable var debugColor: UIColor = UIColor.clear
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        if doesDrawPreview {
+            inflateNib()
+        } else {
+            removeNib()
+            showNibName()
+            showBorder()
+            backgroundColor = debugColor
+        }
     }
     
-    override func prepareForInterfaceBuilder() {
-        self.label.text = "prepared for IB"
-        self.button.titleLabel?.text = "prepared"
+    //MARK: - Class implementation
+    private var nibView: UIView? = nil
+    
+    func inflateNib() {
+        if nibView == nil {
+            nibView = self.fillWithNib()
+        }
+    }
+    
+    func removeNib() {
+        nibView?.removeFromSuperview()
+        nibView = nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        inflateNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        inflateNib()
+    }
+    
+}
+
+extension UIView {
+    
+    func className() -> String {
+        let thisType = type(of: self)
+        return String(describing: thisType)
+    }
+    
+    func fillWithNib(nibName: String) -> UIView? {
+        let bundle = Bundle.init(for: type(of: self))
+        if let view = bundle.loadNibNamed(nibName, owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+            return view
+        }
+        return nil
+    }
+    
+    func fillWithNib() -> UIView? {
+        return fillWithNib(nibName: className())
+    }
+    
+    func showBorder() {
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    func showNibName() {
+        let label = UILabel()
+        label.text = className() + ".xib"
+        label.frame = self.bounds
+        label.textAlignment = .center
+        addSubview(label)
     }
 }
